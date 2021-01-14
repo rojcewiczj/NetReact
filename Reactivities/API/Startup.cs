6a914cf4b8d69650d94c3605c82d2a4363840a86
John Rojcewicz <rojcewiczj@gmail.com>
2021-01-13 23:12:48 -0600

    Added Activities Entity/ API Controller / MediatR query handling

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -39,7 +41,9 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+            
             services.AddControllers();
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddSwaggerGen(c =>
              {
                c.SwaggerDoc("v1", new OpenApiInfo { Title = "api", Version = "v1" });
